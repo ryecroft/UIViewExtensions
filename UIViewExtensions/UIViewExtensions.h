54645b8b0b50cce9daed26243f99eda9cb2f81e3
Stephen Horne <fatste@gmail.com>
2017-11-28 15:35:38 +0000

    Initial Commit

---
+++ UIViewExtensions/UIViewExtensions.h
@@ -0,0 +1,19 @@
+//
+//  UIViewExtensions.h
+//  UIViewExtensions
+//
+//  Created by Stephen Horne on 28/11/17.
+//  Copyright © 2017 Rockfax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIViewExtensions.
+FOUNDATION_EXPORT double UIViewExtensionsVersionNumber;
+
+//! Project version string for UIViewExtensions.
+FOUNDATION_EXPORT const unsigned char UIViewExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIViewExtensions/PublicHeader.h>
+
+
